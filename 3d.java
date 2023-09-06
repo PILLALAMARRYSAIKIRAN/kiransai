01cf3840671d40d8dadad28d523ebb9d9be9b9b7
kiransai <saikiran41078@gmail.com>
2023-09-06 21:42:35 +0530

    commit message

--- 3d.java
+++ 3d.java
@@ -12,3 +12,8 @@ class Demo_3dArr {
 }
 }
 }
+   for(int i= 0;i<2;i++){
+           for(int j= 0;j<2;j++){
+                for(int k=0;k<2;k++){
+          
+        System.out.println("arr["+i+"]["+j+"]["+k+"]="+arr[i][j][k]);
